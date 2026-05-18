166023e508dcf2f69c4b02a5dd131b56ce94447a
KoSax <sxkarajan.0210@gmail.com>
2026-05-18 16:21:21 +0900

    build: restore standard include structure and fix keymap

--- config/include/input_behavior_listener.h
+++
@@ -1,8 +0,0 @@
-#define ZMK_INPUT_BEHAVIOR_LISTENER_NODES(node_id) \
-    input_behavior_listener_##node_id { \
-        compatible = "zmk,input-behavior-listener"; \
-        device = <&vtrackball>; \
-        layers = <0>; \
-    };
-
-#include <dt-bindings/zmk/input_behavior_listener.h>
