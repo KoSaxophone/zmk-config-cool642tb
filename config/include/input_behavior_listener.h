af3d8e5ac702fa90ac1970aefe49f397bd3c4e5f
KoSax <sxkarajan.0210@gmail.com>
2026-05-18 16:06:25 +0900

    fix: patch listener header for v4.1 and simplify keymap

--- config/include/input_behavior_listener.h
+++ config/include/input_behavior_listener.h
@@ -1,1 +1,8 @@
-404: Not Found
+#define ZMK_INPUT_BEHAVIOR_LISTENER_NODES(node_id) \
+    input_behavior_listener_##node_id { \
+        compatible = "zmk,input-behavior-listener"; \
+        device = <&vtrackball>; \
+        layers = <0>; \
+    };
+
+#include <dt-bindings/zmk/input_behavior_listener.h>
